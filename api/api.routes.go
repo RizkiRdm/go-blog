75f9610ed5409726a3e5de740e43c789f8bfd9ae
RizkiRdm <rizkiromdhoni28@gmail.com>
2024-03-15 00:10:40 +0700

    UPDATE - make routing readable

--- api/api.routes.go
+++ api/api.routes.go
@@ -1,17 +1,20 @@
 package api
 
 import (
-	"fmt"
-
+	"github.com/RizkiRdm/go-blog/pkg/handlers"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Routes(r *fiber.App) {
 	api := r.Group("api")
-	V1 := api.Group("V1")
+	V1 := api.Group("v1")
 
-	V1.Get("/", func(c *fiber.Ctx) error {
-		fmt.Println("hello")
-		return nil
-	})
+	// GET ALL BLOGS
+	V1.Get("/blogs", handlers.GetBlogs)
+	// GET DETAIL BLOG
+	V1.Get("/blogs/:id", handlers.GetDetailBlog)
+	// CREATE NEW BLOG
+	V1.Post("/blogs", handlers.CreateBlog)
+	// UPDATE CURRENT BLOG
+	V1.Post("/blogs/:id/:userId", handlers.UpdateBlog)
 }
