b3175d8d47699ba4dc5b712b340779c4370c5354
RizkiRdm <rizkiromdhoni28@gmail.com>
2024-03-17 00:39:41 +0700

    UPDATE - add struct category, and tag on create & update struct

--- pkg/models/blog/models.request.blog.go
+++ pkg/models/blog/models.request.blog.go
@@ -1,14 +1,18 @@
 package blog
 
 type RequestCreateBlog struct {
-	UsernameId int    `json:"username"`
-	Thumbnail  string `json:"thumbnail"`
-	Title      string `json:"title_blog"`
-	Body       string `json:"body_blog"`
+	UsernameId int      `json:"username"`
+	Thumbnail  string   `json:"thumbnail"`
+	Title      string   `json:"title_blog"`
+	Body       string   `json:"body_blog"`
+	Category   string   `json:"cateogory"`
+	Tags       []string `json:"tags"`
 }
 
 type RequestUpdateBlog struct {
-	Thumbnail string `json:"thumbnail"`
-	Title     string `json:"title_blog"`
-	Body      string `json:"body_blog"`
+	Thumbnail string   `json:"thumbnail"`
+	Title     string   `json:"title_blog"`
+	Body      string   `json:"body_blog"`
+	Category  string   `json:"cateogory"`
+	Tags      []string `json:"tags"`
 }
