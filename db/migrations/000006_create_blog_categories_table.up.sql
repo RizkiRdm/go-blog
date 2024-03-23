d2b033c2a23bd3fb073489e3d76fc53426f0cfd8
RizkiRdm <rizkiromdhoni28@gmail.com>
2024-03-23 23:22:36 +0700

    UPDATE - change id to id_blog

--- db/migrations/000006_create_blog_categories_table.up.sql
+++ db/migrations/000006_create_blog_categories_table.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS `blog_categories` (
-    `id` INT NULL,
+    `id_blog` INT NULL,
     `id_category` INT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id_blog`)
 );
 ALTER TABLE `blog_categories`
 ADD FOREIGN KEY (`id`) REFERENCES `blogs`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 ALTER TABLE `blog_categories`
-ADD FOREIGN KEY (`id_category`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON
+ADD FOREIGN KEY (`id_category`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
