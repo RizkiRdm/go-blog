a4ec1e0f5c6d539c6310ad8e6cf19ac73090118c
RizkiRdm <rizkiromdhoni28@gmail.com>
2024-03-25 00:06:25 +0700

    UPDATE - make a upload file & finishing for create blog

--- pkg/handlers/handler.blog.go
+++ pkg/handlers/handler.blog.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"database/sql"
 	"net/http"
+	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/RizkiRdm/go-blog/db"
@@ -36,7 +38,7 @@ func GetBlogs(c *fiber.Ctx) error {
 			blogs.updated_at 
 			FROM blogs 
 			LEFT JOIN users ON blogs.user_id = users.id 
-			LEFT JOIN blog_categories ON blogs.id = blog_categories.id
+			LEFT JOIN blog_categories ON blogs.id = blog_categories.id_blog
 			LEFT JOIN categories ON blog_categories.id_category = categories.id
 			LEFT JOIN blog_tags ON blogs.id = blog_tags.id_blog 
 			LEFT JOIN tags ON blog_tags.id_tag = tags.id  
@@ -81,7 +83,7 @@ func GetDetailBlog(c *fiber.Ctx) error {
 
 	var blog blog.BlogResponse
 	var tag string
-	q := "SELECT blogs.id, users.username, GROUP_CONCAT(DISTINCT categories.title) AS kategori, GROUP_CONCAT(DISTINCT tags.name) AS tag, blogs.thumbnail, blogs.title, blogs.body, blogs.created_at, blogs.updated_at FROM blogs LEFT JOIN users ON blogs.user_id = users.id LEFT JOIN blog_categories ON blogs.id = blog_categories.id LEFT JOIN categories ON blog_categories.id_category = categories.id LEFT join blog_tags ON blogs.id = blog_tags.id_tag LEFT JOIN tags ON blogs.id = tags.id WHERE blogs.id = ?"
+	q := "SELECT blogs.id, users.username, categories.title AS kategori, GROUP_CONCAT(DISTINCT tags.name) AS tag, blogs.thumbnail, blogs.title, blogs.body, blogs.created_at, blogs.updated_at FROM blogs LEFT JOIN users ON blogs.user_id = users.id LEFT JOIN blog_categories ON blogs.id = blog_categories.id_blog LEFT JOIN categories ON blog_categories.id_category = categories.id LEFT join blog_tags ON blogs.id = blog_tags.id_tag LEFT JOIN tags ON blogs.id = tags.id WHERE blogs.id = ?"
 	err := db.QueryRow(q, id).Scan(&blog.Id, &blog.Username, &blog.Category, &tag, &blog.Title, &blog.Thumbnail, &blog.Body, &blog.CreatedAt, &blog.UpdatedAt)
 
 	if err != nil {
@@ -104,7 +106,6 @@ func GetDetailBlog(c *fiber.Ctx) error {
 }
 
 // create new blog - POST
-
 func CreateBlog(c *fiber.Ctx) error {
 	blogReq := new(blog.RequestCreateBlog)
 	if err := c.BodyParser(blogReq); err != nil {
@@ -112,4 +113,92 @@ func CreateBlog(c *fiber.Ctx) error {
 			"message": err.Error(),
 		})
 	}
+
+	// convert user_id from string to int
+	userId, err := strconv.Atoi(blogReq.UsernameId)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"message": "invalid user ID",
+		})
+	}
+
+	// handle image upload
+	file, err := c.FormFile("thumbnail")
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"message": "thumbnail upload error",
+		})
+	}
+
+	// save image to uploads directory
+	thumbnailPath := filepath.Join("./uploads", file.Filename)
+	if err := c.SaveFile(file, thumbnailPath); err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"message": "failed save image",
+		})
+	}
+
+	// define db variable
+	db := db.Connection()
+	defer db.Close()
+
+	// start transaction
+	tx, err := db.Begin()
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	// insert blog
+	blogQuery := "INSERT INTO `blogs` (`user_id`, `title`, `thumbnail`, `body`) VALUES (?, ?, ?, ?)"
+	result, err := tx.Exec(blogQuery, userId, blogReq.Title, thumbnailPath, blogReq.Body)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"message": "failed insert blog",
+		})
+	}
+	blogId, err := result.LastInsertId()
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to retrieve blog ID",
+		})
+	}
+
+	// insert blogId to table blog_categories
+	// convert category_id from string to int
+	categoryId, err := strconv.Atoi(blogReq.CategoryId)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"message": "invalid category ID",
+		})
+	}
+	categoryQuery := "INSERT INTO `blog_categories` (`id_blog`, `id_category`) VALUES (?, ?) ON DUPLICATE KEY UPDATE id_blog = id_blog"
+	if _, err := tx.Exec(categoryQuery, blogId, categoryId); err != nil {
+		tx.Rollback()
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to insert category",
+		})
+	}
+
+	// insert blog_id to blog_tags
+	tagQuery := "INSERT INTO `blog_tags` (`id_blog`, `id_tag`) VALUES (?, ?) ON DUPLICATE KEY UPDATE id_blog = id_blog"
+	for _, tagStr := range blogReq.TagsId {
+		// convert tags_id to int
+		tagId, err := strconv.Atoi(tagStr)
+		if err != nil {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"message": "invalid tags id",
+			})
+		}
+		if _, err := tx.Exec(tagQuery, blogId, tagId); err != nil {
+			tx.Rollback()
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"message": "Failed to insert tags"})
+		}
+	}
+
+	return c.Status(http.StatusCreated).JSON(fiber.Map{
+		"message": "success",
+		"data":    blogReq,
+	})
 }
