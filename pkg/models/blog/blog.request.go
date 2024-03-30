e085e59907e03c00fcaecd27cac9121ab3768eca
RizkiRdm <rizkiromdhoni28@gmail.com>
2024-03-30 22:37:47 +0700

    UPDATE - update filename

--- pkg/models/blog/blog.request.go
+++ pkg/models/blog/blog.request.go
@@ -1,12 +1,12 @@
 package blog
 
 type RequestCreateBlog struct {
-	UsernameId string   `json:"user_id"`
-	Thumbnail  string   `json:"thumbnail"`
-	Title      string   `json:"title"`
-	Body       string   `json:"body"`
-	CategoryId string   `json:"category_id"`
-	TagsId     []string `json:"tags_id"`
+	UsernameId int    `json:"user_id"`
+	Thumbnail  string `json:"thumbnail"`
+	Title      string `json:"title"`
+	Body       string `json:"body"`
+	CategoryId int    `json:"category_id"`
+	TagsId     []int  `json:"tags_id"`
 }
 
 type RequestUpdateBlog struct {
