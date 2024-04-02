7e57b75411ac015669feb9b0c5ee261347791b83
RizkiRdm <rizkiromdhoni28@gmail.com>
2024-04-03 00:17:17 +0700

    UPDATE - change package name, struct & add new struct for claims JWT

--- pkg/models/users/users.go
+++ pkg/models/users/users.go
@@ -1,12 +1,21 @@
-package models
+package users
 
-import "time"
+import (
+	"time"
 
-type Users struct {
+	"github.com/golang-jwt/jwt/v4"
+)
+
+type User struct {
 	Id        uint      `json:"id"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
 	Username  string    `json:"username"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Claims struct {
+	Email string `json:"email"`
+	jwt.RegisteredClaims
 }
