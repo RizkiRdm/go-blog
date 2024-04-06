6f2f382160056cf97c211c96b6e5d641f6b14469
RizkiRdm <rizkiromdhoni28@gmail.com>
2024-04-07 00:19:38 +0700

    FEAT - add request for login

--- pkg/models/users/users.request.go
+++ pkg/models/users/users.request.go
@@ -2,14 +2,20 @@ package users
 
 // create request
 type UsersCreateRequest struct {
-	Name     string `json:"name" `
-	Username string `json:"username" `
-	Password string `json:"password" `
-	Email    string `json:"email" `
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 // update request
 type UsersUpdateRequest struct {
 	Name     string `json:"name"`
 	Username string `json:"username"`
+}
+
+// login request
+type UserLoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
