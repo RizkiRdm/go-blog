51e6a8c0ffca6e66e8760dc8371ce8ae0d8eac63
RizkiRdm <rizkiromdhoni28@gmail.com>
2024-04-09 11:32:42 +0700

    FEAT - add route for authentication user

--- api/routes.go
+++ api/routes.go
@@ -10,21 +10,23 @@ func Routes(r *fiber.App) {
 	api := r.Group("api")
 	V1 := api.Group("v1")
 
+	// AUTH ROUTES
 	// LOGIN USER
 	V1.Post("/login", users.LoginUser)
 	// REGISTER USER
 	V1.Post("/register", users.RegisterUser)
+	// LOGOUT USER
+	V1.Post("/logout", users.LogoutUser)
+
+	// BLOG ROUTES
 	// GET ALL BLOGS
 	V1.Get("/blogs", blog.GetBlogs)
 	// GET DETAIL BLOG
 	V1.Get("/blogs/:id", blog.GetDetailBlog)
 	// CREATE NEW BLOG
-	// V1.Post("/blogs", middleware.AuthMiddleware(), blog.HandleBlogWithDetails)
 	V1.Post("/blogs", blog.CreateBlog)
-
 	// CREATE NEW CATEGORY
 	V1.Post("/categories", blog.CreateNewCategory)
-
 	// REGISTER USER
 	V1.Post("/register", users.RegisterUser)
 }
