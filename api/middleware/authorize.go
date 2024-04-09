ee6088a1293fbc8612e157cc9fb354952f5a906e
RizkiRdm <rizkiromdhoni28@gmail.com>
2024-04-09 11:32:06 +0700

    FEAT - make middleware for user to create new blog

---
+++ api/middleware/authorize.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/RizkiRdm/go-blog/utils"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Midleware(c *fiber.Ctx) error {
+	authHeader := c.Get("Authorization")
+	if authHeader == "" {
+		return c.Status(http.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Missing authorization header ",
+		})
+	}
+
+	tokenParts := strings.Split(authHeader, "")
+	if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+		return c.Status(http.StatusUnauthorized).JSON(fiber.Map{
+			"message": "invalid token format",
+		})
+	}
+
+	claims, err := utils.VerifyTokenJWT(tokenParts[1])
+	if err != nil {
+		return c.Status(http.StatusUnauthorized).JSON(fiber.Map{
+			"message":    "invalid token",
+			"messageErr": err.Error(),
+		})
+	}
+	c.Locals("user", claims.Email)
+	return c.Next()
+}
