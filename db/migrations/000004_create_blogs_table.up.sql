c597aeeb6ab93d26561490651c47a3c6ac3f4091
RizkiRdm <rizkiromdhoni28@gmail.com>
2024-04-10 20:45:40 +0700

    FEAT - add new structure for blog

--- db/migrations/000004_create_blogs_table.up.sql
+++ db/migrations/000004_create_blogs_table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS `blogs` (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     title VARCHAR(100) NOT NULL,
+    slug VARCHAR(100) NOT NULL,
     thumbnail VARCHAR(100),
     body TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
