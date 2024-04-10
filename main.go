56d3f24a94908df2605212a54a17534c105e1539
RizkiRdm <rizkiromdhoni28@gmail.com>
2024-04-10 20:45:17 +0700

    FEAT - make cors

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/RizkiRdm/go-blog/api"
 	"github.com/RizkiRdm/go-blog/db"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -13,7 +14,14 @@ func main() {
 	// initalize fiber route
 	app := fiber.New()
 
+	// setup CORS
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: false,
+		AllowOrigins:     "*",
+	}))
+
 	api.Routes(app)
 
+	// app.Use(middleware.Midleware())
 	app.Listen(":8000")
 }
