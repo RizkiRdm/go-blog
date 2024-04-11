714aff0a3a8e8a4e945deb0bf6a2b4cf41cfbd12
RizkiRdm <rizkiromdhoni28@gmail.com>
2024-04-11 16:43:17 +0700

    FEAT - add struct for response

--- pkg/models/category/category.go
+++ pkg/models/category/category.go
@@ -2,13 +2,12 @@ package category
 
 import "time"
 
-type categories struct {
+type Categories struct {
 	Id        uint      `json:"id"`
 	Name      string    `json:"category_name"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
-type categoryResponse struct {
-	Name      string    `json:"category_name"`
-	CreatedAt time.Time `json:"created_at"`
+type CategoryResponse struct {
+	Name string `json:"category_name"`
 }
